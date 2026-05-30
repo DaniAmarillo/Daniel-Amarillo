64b865845efa13181aff082007650eee4d272bc9
Luguerreroo <lfguerreroor@gmail.com>
2026-05-30 14:46:00 -0500

    :bug: correción taller parcial1 Luisa Guerrero

--- grupo_1/guerrero_luisa/parcial_01/output/bdcc_2025_schema.sql
+++ grupo_1/guerrero_luisa/parcial_01/output/bdcc_2025_schema.sql
@@ -1,47 +1,30 @@
 
-PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS papers;
+DROP TABLE IF EXISTS references_table;
 
 CREATE TABLE papers (
-  paper_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  journal_name TEXT,
-  title TEXT NOT NULL,
-  authors_raw TEXT,
-  publication_date TEXT,
-  year INTEGER,
-  doi TEXT UNIQUE,
-  url TEXT,
-  abstract TEXT,
-  n_authors INTEGER,
-  citations INTEGER,
-  views INTEGER,
-  downloads INTEGER,
-  n_references INTEGER,
-  topic_label TEXT
-);
-
-CREATE TABLE authors (
-  author_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_name TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE paper_authors (
-  paper_id INTEGER,
-  author_id INTEGER,
-  author_order INTEGER,
-  FOREIGN KEY (paper_id) REFERENCES papers(paper_id),
-  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    paper_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    journal_name TEXT,
+    title TEXT NOT NULL,
+    publication_date TEXT,
+    year INTEGER,
+    doi TEXT,
+    url TEXT,
+    abstract TEXT,
+    authors_raw TEXT,
+    n_authors INTEGER,
+    citations INTEGER,
+    downloads INTEGER,
+    n_references INTEGER,
+    topic_label TEXT
 );
 
 CREATE TABLE references_table (
-  reference_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  reference_text_normalized TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE paper_references (
-  paper_id INTEGER,
-  reference_id INTEGER,
-  reference_order INTEGER,
-  FOREIGN KEY (paper_id) REFERENCES papers(paper_id),
-  FOREIGN KEY (reference_id) REFERENCES references_table(reference_id)
+    reference_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    doi TEXT,
+    title TEXT,
+    reference_order INTEGER,
+    reference_text TEXT,
+    reference_text_normalized TEXT
 );
 
