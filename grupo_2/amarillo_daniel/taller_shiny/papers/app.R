bae29189969ed6baf26868e16a3fff51de44f758
PC Paula <damoech@gmail.com>
2026-06-04 13:12:09 -0500

    papers velocimetro -poner abstract titulo y especificaciones-

--- grupo_2/amarillo_daniel/taller_shiny/papers/app.R
+++ grupo_2/amarillo_daniel/taller_shiny/papers/app.R
@@ -1,7 +1,7 @@
 
 paquetes <- c( 
   "RSQLite","knitr","tidyverse","knitr","highcharter"
-  ,"bslib","tidyverse","lubridate","DT","bsicons"
+  ,"bslib","tidyverse","lubridate","DT","bsicons","plotly"
 
 )
 
@@ -36,7 +36,7 @@ library(shiny)
 ui <- fluidPage(page_navbar( 
   sidebar = sidebar(
     title = "Controles Generales",
-    sliderInput("year", "Año:", min = min(papers$year), max = max(papers$year), value = c(2024,2026))
+    sliderInput("year", "Año:", min = min(papers$year), max = max(papers$year), value = c(2024,2026)),
     ),
 ###### pestaña general #####
     nav_panel(
@@ -53,7 +53,7 @@ ui <- fluidPage(page_navbar(
                   wellPanel(value_box(
                     title = "referencias promedio",
                     value =  h3(textOutput(outputId = "mean_referencias")),
-                    showcase = bs_icon("book",fill = "rgb(119,152,191) !important"),
+                    showcase = bs_icon("book",fill = "rgb(119,171,67) !important"),
                     p("por paper"),
                     styles = list(header = "font-size: 0.9rem; font-weight: bold;")
                   ))
@@ -61,7 +61,7 @@ ui <- fluidPage(page_navbar(
                 fluidRow(wellPanel(value_box(
                   title = "# promedio de citas",
                   value =  h3(textOutput(outputId = "mean_citas")),
-                  showcase = bs_icon("Flag",fill = "rgb(119,152,191) !important"),
+                  showcase = bs_icon("Flag",fill = "rgb(119,171,67) !important"),
                   p("por paper"),
                   styles = list(header = "font-size: 0.9rem; font-weight: bold;")
                 )))),
@@ -71,7 +71,7 @@ ui <- fluidPage(page_navbar(
                     wellPanel(value_box(
                       title = "# de papers",
                       value =  h3(textOutput(outputId = "n_papers")),
-                      showcase = bs_icon("file-text",fill = "rgb(119,152,191) !important"), 
+                      showcase = bs_icon("file-text",fill = "rgb(119,171,67) !important"), 
                       p("publicados"),
                       styles = list(header = "font-size: 0.9rem; font-weight: bold;")
                     ))
@@ -79,7 +79,7 @@ ui <- fluidPage(page_navbar(
                   fluidRow(wellPanel(value_box(
                     title = "FCWI promedio",
                     value =  h3(textOutput(outputId = "mean_fwci")),
-                    showcase = bs_icon("graph-up",fill = "rgb(119,152,191) !important"),
+                    showcase = bs_icon("graph-up",fill = "rgb(119,171,67) !important"),
                     p("impacto"),
                     styles = list(header = "font-size: 0.9rem; font-weight: bold;")
                   ))))
@@ -93,21 +93,40 @@ ui <- fluidPage(page_navbar(
 ###### Papers ######
     nav_panel(title="Papers", 
               h3("Información de papers"),
-              selectInput("paper_title","Paper:",papers$titulo),
+              selectInput("paper_title","Paper:",choices = NULL),
               layout_columns(
                 col_widths = c(4,4,4),
                 column(
                   width = 12,
                   fluidRow(
                     wellPanel(value_box(
-                      title = "referencias promedio",
-                      value =  h3(textOutput(outputId = "mean_referencias")),
-                      showcase = bs_icon("book",fill = "rgb(119,152,191) !important"),
-                      p("por paper"),
+                      title = "",
+                      value =  highchartOutput("gauge_papers_fcwi"),
+                      styles = list(header = "font-size: 0.9rem; font-weight: bold;")
+                    ))
+                  ),
+                  fluidRow(
+                    wellPanel(value_box(
+                      title = "",
+                      value =  highchartOutput("gauge_papers_auth"),
                       styles = list(header = "font-size: 0.9rem; font-weight: bold;")
                     ))
                   )
-              )
+              ),column(width = 12, 
+                       fluidRow(
+                         wellPanel(value_box(
+                           title = "",
+                           value =  highchartOutput("gauge_papers_reff"),
+                           styles = list(header = "font-size: 0.9rem; font-weight: bold;")
+                         ))
+                       ),
+                       fluidRow(
+                         wellPanel(value_box(
+                           title = "",
+                           value =  highchartOutput("gauge_papers_citas"),
+                           styles = list(header = "font-size: 0.9rem; font-weight: bold;")
+                         ))
+                       ))
               )), 
 ###### Autores #####
     nav_panel("Autores", "Page C content"), 
@@ -130,7 +149,7 @@ ui <- fluidPage(page_navbar(
 
 #### server ####
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
 ####  filtro años ####
 papers_year_gen <- reactive({
   req(input$year)
@@ -225,7 +244,128 @@ papers_paper_info <- reactive({
         class = 'cell-border stripe compact'
       )
     })
-####  ####
+####observador selector papers####
+
+observe({
+  # Extraemos la columna de interés de nuestra tabla reactiva
+  df <- papers_year_gen()
+  opciones_nuevas <- unique(df$titulo)
+  
+  # 5. Actualizamos el selector de la UI de forma eficiente
+  updateSelectInput(session, "paper_title",
+                    choices = opciones_nuevas,
+                    selected = head(opciones_nuevas, 1))
+})
+
+
+#### velocimetro papers (fcwi) ####
+output$gauge_papers_fcwi <- renderHighchart({
+ 
+ papersg_fcwi <- papers_year_gen()
+ 
+ df_gauge <- papersg_fcwi |> filter(titulo == input$paper_title)
+ 
+  
+ highchart() %>%
+    hc_chart(type = "gauge", plotBackgroundColor = NULL, plotBackgroundImage = NULL, plotBorderWidth = 0, plotShadow = FALSE) %>%
+    hc_title(text = "FCWI") %>%
+    hc_pane(startAngle = -90, endAngle = 90,
+            background = list(
+              list(backgroundColor = "#FFF", borderWidth = 0, outerRadius = "109%", innerRadius = "107%")
+            )) %>%
+    hc_yAxis(min = min(papers$fwci), max = max(papers$fwci),
+             minorTickInterval = "auto", minorTickWidth = 1, minorTickLength = 10, minorTickPosition = "inside", minorTickColor = "#666",
+             tickPixelInterval = 30, tickWidth = 2, tickPosition = "inside", tickLength = 10, tickColor = "#666",
+             labels = list(step = 2, rotation = "auto"),
+             plotBands = list(
+               list(from = min(papers$fwci), to = 10, color = "#DF5353"),    # Verde
+               list(from = 10, to = max(papers$fwci)*0.8, color = "#DDDF0D"),   # Amarillo
+               list(from = max(papers$fwci)*0.8, to = max(papers$fwci), color = "#55BF3B")   # Rojo
+             )) %>%
+    hc_add_series(name = "Valor", data = list(round(df_gauge$fwci,2)))
+})
+#### valocimetro papers (autores) ####
+output$gauge_papers_auth <- renderHighchart({
+  
+  papersg_auth <- papers_year_gen()
+  
+  df_gauge <- papersg_auth |> filter(titulo == input$paper_title)
+  
+  
+  highchart() %>%
+    hc_chart(type = "gauge", plotBackgroundColor = NULL, plotBackgroundImage = NULL, plotBorderWidth = 0, plotShadow = FALSE) %>%
+    hc_title(text = "Autores") %>%
+    hc_pane(startAngle = -90, endAngle = 90,
+            background = list(
+              list(backgroundColor = "#FFF", borderWidth = 0, outerRadius = "109%", innerRadius = "107%")
+            )) %>%
+    hc_yAxis(min = min(papers$n_autores), max = max(papers$n_autores),
+             minorTickInterval = "auto", minorTickWidth = 1, minorTickLength = 10, minorTickPosition = "inside", minorTickColor = "#666",
+             tickPixelInterval = 30, tickWidth = 2, tickPosition = "inside", tickLength = 10, tickColor = "#666",
+             labels = list(step = 2, rotation = "auto"),
+             plotBands = list(
+               list(from = min(papers$n_autores), to = 10, color = "#DF5353"),    # Verde
+               list(from = 10, to = max(papers$n_autores)*0.8, color = "#DDDF0D"),   # Amarillo
+               list(from = max(papers$n_autores)*0.8, to = max(papers$n_autores), color = "#55BF3B")   # Rojo
+             )) %>%
+    hc_add_series(name = "Valor", data = list(round(df_gauge$n_autores,2)),
+                  tooltip = list(valueSuffix = " autores"))
+})
+#### valocimetro papers (autores) ####
+output$gauge_papers_reff <- renderHighchart({
+  
+  papersg_reff <- papers_year_gen()
+  
+  df_gauge <- papersg_reff |> filter(titulo == input$paper_title)
+  
+  
+  highchart() %>%
+    hc_chart(type = "gauge", plotBackgroundColor = NULL, plotBackgroundImage = NULL, plotBorderWidth = 0, plotShadow = FALSE) %>%
+    hc_title(text = "Referencias") %>%
+    hc_pane(startAngle = -90, endAngle = 90,
+            background = list(
+              list(backgroundColor = "#FFF", borderWidth = 0, outerRadius = "109%", innerRadius = "107%")
+            )) %>%
+    hc_yAxis(min = min(papers$n_referencia), max = max(papers$n_referencia),
+             minorTickInterval = "auto", minorTickWidth = 1, minorTickLength = 10, minorTickPosition = "inside", minorTickColor = "#666",
+             tickPixelInterval = 30, tickWidth = 2, tickPosition = "inside", tickLength = 10, tickColor = "#666",
+             labels = list(step = 2, rotation = "auto"),
+             plotBands = list(
+               list(from = min(papers$n_referencia), to = 10, color = "#DF5353"),    # Verde
+               list(from = 10, to = max(papers$n_referencia)*0.8, color = "#DDDF0D"),   # Amarillo
+               list(from = max(papers$n_referencia)*0.8, to = max(papers$n_referencia), color = "#55BF3B")   # Rojo
+             )) %>%
+    hc_add_series(name = "Valor", data = list(round(df_gauge$n_referencia,2)),
+                  tooltip = list(valueSuffix = " referencias"))
+})
+#### valocimetro papers (citaciones) ####
+
+output$gauge_papers_citas <- renderHighchart({
+  
+  papersg_reff <- papers_year_gen()
+  
+  df_gauge <- papersg_reff |> filter(titulo == input$paper_title)
+  
+  
+  highchart() %>%
+    hc_chart(type = "gauge", plotBackgroundColor = NULL, plotBackgroundImage = NULL, plotBorderWidth = 0, plotShadow = FALSE) %>%
+    hc_title(text = "Citas") %>%
+    hc_pane(startAngle = -90, endAngle = 90,
+            background = list(
+              list(backgroundColor = "#FFF", borderWidth = 0, outerRadius = "109%", innerRadius = "107%")
+            )) %>%
+    hc_yAxis(min = min(papers$nro_de_citas), max = max(papers$nro_de_citas),
+             minorTickInterval = "auto", minorTickWidth = 1, minorTickLength = 10, minorTickPosition = "inside", minorTickColor = "#666",
+             tickPixelInterval = 30, tickWidth = 2, tickPosition = "inside", tickLength = 10, tickColor = "#666",
+             labels = list(step = 2, rotation = "auto"),
+             plotBands = list(
+               list(from = min(papers$nro_de_citas), to = 10, color = "#DF5353"),    # Verde
+               list(from = 10, to = max(papers$nro_de_citas)*0.8, color = "#DDDF0D"),   # Amarillo
+               list(from = max(papers$nro_de_citas)*0.8, to = max(papers$nro_de_citas), color = "#55BF3B")   # Rojo
+             )) %>%
+    hc_add_series(name = "Valor", data = list(round(df_gauge$nro_de_citas,2)),
+                  tooltip = list(valueSuffix = " citaciones"))
+})
 }
 
 # Run the application 
