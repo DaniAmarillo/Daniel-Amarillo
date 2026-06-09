3319d5d9dfc046e741d18f274e5ed205ad2c04c8
Mendivenson <mbarraganz@unal.edu.co>
2026-06-09 02:06:31 -0500

    Entrega parcial 2

---
+++ grupo_2/barragan_michel/parcial_2/scripts/test_update.R
@@ -0,0 +1,10 @@
+test_update <- function() {
+  
+  Sys.sleep(10)
+  
+  list(
+    message = 'Due to scrapping problems related to Shiny,\nBigQuery database is not being updated.',
+    duration = 10
+  )
+  
+}
